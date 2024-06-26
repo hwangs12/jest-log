4e575ca34f50d86cbc634027d75484bbb17b4771
hwangs12 <sungjun94@gmail.com>
2024-06-26 17:10:21 -0400

    test test test

--- src/test/Utils.spec.ts
+++ src/test/Utils.spec.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
     test("should return uppercase", () => {
@@ -11,5 +11,23 @@ describe("Utils test suite", () => {
 
         // assertion:
         expect(actual).toBe(expected);
+    });
+
+    it("should return uppercase of valid string", () => {
+        const sut = toUpperCase;
+        const expected = "ABC";
+
+        const actual = sut("abc");
+
+        expect(actual).toBe(expected);
+    });
+
+    it("should return info for valid string", () => {
+        const actual = getStringInfo("My-String");
+
+        expect(actual.lowerCase).toBe("my-string");
+        expect(actual.extraInfo).toEqual({}); //toBe won't work on object equality
+        expect(actual.characters.length).toBe(9);
+        expect(actual.characters).toHaveLength(9);
     });
 });
