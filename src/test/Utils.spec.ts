e007536c7d7f787a069f4c03d7098322de78f2a5
hwangs12 <sungjun94@gmail.com>
2024-06-26 21:46:57 -0400

    test

--- src/test/Utils.spec.ts
+++ src/test/Utils.spec.ts
@@ -22,6 +22,31 @@ describe("Utils test suite", () => {
         expect(actual).toBe(expected);
     });
 
+    describe("getStringInfo for arg My-String should", () => {
+        test("return right length", () => {
+            const actual = getStringInfo("My-String");
+            expect(actual.characters).toHaveLength(9);
+        });
+        test("return right lower case", () => {
+            const actual = getStringInfo("My-String");
+            expect(actual.lowerCase).toBe("my-string");
+        });
+        test("return right upper case", () => {
+            const actual = getStringInfo("My-String");
+            expect(actual.upperCase).toBe("MY-STRING");
+        });
+        test("return right characters", () => {
+            const actual = getStringInfo("My-String");
+            expect(actual.characters).toEqual(["M", "y", "-", "S", "t", "r", "i", "n", "g"]);
+            expect(actual.characters).toContain<string>("M");
+            expect(actual.characters).toEqual(expect.arrayContaining(["S", "t", "r", "i", "n", "g", "M", "y", "-"]));
+        });
+        test("return defined extra info", () => {
+            const actual = getStringInfo("My-String");
+            expect(actual.extraInfo).toBeDefined();
+        });
+    });
+
     it("should return info for valid string", () => {
         const actual = getStringInfo("My-String");
 
@@ -29,5 +54,10 @@ describe("Utils test suite", () => {
         expect(actual.extraInfo).toEqual({}); //toBe won't work on object equality
         expect(actual.characters.length).toBe(9);
         expect(actual.characters).toHaveLength(9);
+
+        expect(actual.extraInfo).not.toBe(undefined);
+        expect(actual.extraInfo).not.toBeUndefined();
+        expect(actual.extraInfo).toBeDefined();
+        expect(actual.extraInfo).toBeTruthy();
     });
 });
