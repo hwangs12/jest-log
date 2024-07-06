ab263abe370804eca6bdbebcc4b9abcf8d087597
hwangs12 <sungjun94@gmail.com>
2024-07-06 05:22:20 -0400

    fakes

--- src/app/doubles/OtherUtils.ts
+++ src/app/doubles/OtherUtils.ts
@@ -6,6 +6,18 @@ export type stringInfo = {
     extraInfo: Object | undefined;
 };
 
+type LoggerServiceCallBack = (arg: string) => void;
+
 export function calculateComplexity(stringInfo: stringInfo) {
     return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
+}
+
+/* fakes */
+export function toUpperCaseWithCb(arg: string, callback: LoggerServiceCallBack) {
+    if (!arg) {
+        callback("invalid argument!");
+        return;
+    }
+    callback(`called function with ${arg}`);
+    return arg.toUpperCase();
 }
