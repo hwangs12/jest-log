97fcb135b5efb546bdfcce9ce3c939d3aaaf7dd6
hwangs12 <sungjun94@gmail.com>
2024-07-19 15:41:11 -0400

    mock elegance

--- src/test/server_app/data/UserCredentialsDataAccess.spec.ts
+++ src/test/server_app/data/UserCredentialsDataAccess.spec.ts
@@ -1,20 +1,31 @@
 import { DataBase } from "../../../app/server_app/data/DataBase";
 import { UserCredentialsDataAccess } from "../../../app/server_app/data/UserCredentialsDataAccess";
+import { Account } from "../../../app/server_app/model/AuthModel";
 
 const insertMock = jest.fn();
 const getByMock = jest.fn();
 
 jest.mock("../../../app/server_app/data/DataBase", () => {
     return {
-        DataBase: {
-            insert: insertMock,
-            getBy: getByMock,
-        },
+        DataBase: jest.fn().mockImplementation(() => {
+            return {
+                insert: insertMock,
+                getBy: getByMock,
+            };
+        }),
     };
 });
 
 describe("UserCredentialsDataAccess test suite", () => {
     let sut: UserCredentialsDataAccess;
+
+    const someAccount: Account = {
+        id: "",
+        password: "somePassword",
+        userName: "someUserName",
+    };
+
+    const someId = "1234";
 
     beforeEach(() => {
         sut = new UserCredentialsDataAccess();
@@ -23,5 +34,13 @@ describe("UserCredentialsDataAccess test suite", () => {
 
     afterEach(() => {
         jest.clearAllMocks();
+    });
+
+    test("should add user and return the id", async () => {
+        insertMock.mockResolvedValueOnce(someId);
+        const actualId = await sut.addUser(someAccount);
+
+        expect(actualId).toBe(someId);
+        expect(insertMock).toHaveBeenCalledWith(someAccount);
     });
 });
