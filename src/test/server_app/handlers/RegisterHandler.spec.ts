575ccc77cfdc61899de00a3c1ac88728bb4c6afb
hwangs12 <sungjun94@gmail.com>
2024-07-21 21:23:45 -0400

    resolved value

--- src/test/server_app/handlers/RegisterHandler.spec.ts
+++ src/test/server_app/handlers/RegisterHandler.spec.ts
@@ -1,4 +1,14 @@
 import { RegisterHandler } from "../../../app/server_app/handlers/RegisterHandler";
+import { IncomingMessage, ServerResponse } from "http";
+import { Authorizer } from "../../../app/server_app/auth/Authorizer";
+import { HTTP_CODES, HTTP_METHODS } from "../../../app/server_app/model/ServerModel";
+import { Account } from "../../../app/server_app/model/AuthModel";
+
+const getRequestBodyMock = jest.fn();
+
+jest.mock("../../../app/server_app/utils/Utils", () => ({
+    getRequestBody: () => getRequestBodyMock(),
+}));
 
 describe("RegisterHandler test suite", () => {
     let sut: RegisterHandler;
@@ -6,10 +16,45 @@ describe("RegisterHandler test suite", () => {
         method: undefined,
     };
 
-    const responseMock = {};
+    const responseMock = {
+        statusCode: 0,
+        writeHead: jest.fn(),
+        write: jest.fn(),
+    };
 
-    const authorizerMock = {};
+    const authorizerMock = {
+        registerUser: jest.fn(),
+    };
+
+    const someAccount: Account = {
+        id: "",
+        password: "somePassword",
+        userName: "someUserName",
+    };
+
+    const someId = "1234";
+
     beforeEach(() => {
-        sut = new RegisterHandler(request as any, responseMock as any, authorizerMock as any);
+        sut = new RegisterHandler(request as IncomingMessage, responseMock as any as ServerResponse, authorizerMock as any as Authorizer);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test("should register valid accounts in requests", async () => {
+        request.method = HTTP_METHODS.POST;
+        getRequestBodyMock.mockResolvedValueOnce(someAccount);
+        authorizerMock.registerUser.mockResolvedValueOnce(someId);
+
+        await sut.handleRequest();
+
+        expect(responseMock.statusCode).toBe(HTTP_CODES.CREATED);
+        expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.CREATED, { "Content-Type": "application/json" });
+        expect(responseMock.write).toBeCalledWith(
+            JSON.stringify({
+                userId: someId,
+            })
+        );
     });
 });
