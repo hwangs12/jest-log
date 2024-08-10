bcc7b3cf804b6fd6bb656ff2bb6550a4c94e0d07
hwangs12 <sungjun94@gmail.com>
2024-08-09 21:47:40 -0400

    before witin describe describe

--- src/test/server_app/handlers/ReservationsHandler.spec.ts
+++ src/test/server_app/handlers/ReservationsHandler.spec.ts
@@ -67,42 +67,9 @@ describe("Reservation Handler Test Suite", () => {
         responseMock.statusCode = 0;
     });
 
-    test("should return token for valid accounts in requests", async () => {
-        request.method = HTTP_METHODS.POST;
-        getRequestBodyMock.mockResolvedValueOnce(someAccount);
-        authorizerMock.login.mockResolvedValueOnce(someToken);
-        await sut.handleRequest();
-        expect(authorizerMock.login).toBeCalledWith(someAccount.userName, someAccount.password);
-        expect(responseMock.statusCode).toBe(HTTP_CODES.CREATED);
-        expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.CREATED, { "Content-Type": "application/json" });
-        expect(responseMock.write).toBeCalledWith(JSON.stringify({ token: someToken }));
-    });
-
-    test("should return not found for invalid accounts in requests", async () => {
-        request.method = HTTP_METHODS.POST;
-        getRequestBodyMock.mockResolvedValueOnce(someAccount);
-        authorizerMock.login.mockResolvedValueOnce(undefined);
-        await sut.handleRequest();
-
-        expect(authorizerMock.login).toBeCalledWith(someAccount.userName, someAccount.password);
-        expect(responseMock.statusCode).toBe(HTTP_CODES.NOT_fOUND);
-        expect(responseMock.write).toBeCalledWith(JSON.stringify("wrong username or password"));
-    });
-
-    test("should return bad request for invalid requests", async () => {
-        request.method = HTTP_METHODS.POST;
-        getRequestBodyMock.mockResolvedValueOnce({});
-        await sut.handleRequest();
-        expect(authorizerMock.login).not.toBeCalled();
-        expect(responseMock.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
-        expect(responseMock.write).toBeCalledWith(JSON.stringify("userName and password required"));
-    });
-    test("should do nothing for not supported http methods", async () => {
-        request.method = HTTP_METHODS.GET;
-        await sut.handleRequest();
-
-        expect(responseMock.writeHead).not.toBeCalled();
-        expect(responseMock.write).not.toBeCalled();
-        expect(getRequestBodyMock).not.toBeCalled();
+    describe("POST reqeusts", () => {
+        beforeEach(() => {
+            request.method = HTTP_METHODS.POST;
+        });
     });
 });
