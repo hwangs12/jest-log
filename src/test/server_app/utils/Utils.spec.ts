dee337ef4a206d5d7118248bf90067952ea12826
hwangs12 <sungjun94@gmail.com>
2024-08-24 22:12:37 -0400

    object

--- src/test/server_app/utils/Utils.spec.ts
+++ src/test/server_app/utils/Utils.spec.ts
@@ -1,3 +1,15 @@
+const requestMock = {
+    on: jest.fn(),
+};
+
+const someObject = {
+    name: "John",
+    age: 30,
+    city: "Paris",
+};
+
 describe("getRequestBody test suite", () => {
-    it("should return object for valid JS");
+    test("should return object for valid JS");
+    test("should throw error for invalid JSON");
+    test("should throw error for unexpected error");
 });
