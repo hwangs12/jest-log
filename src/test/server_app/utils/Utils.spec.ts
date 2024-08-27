87b8004be92d0d047b4acd2a0008518fbff0fb9a
hwangs12 <sungjun94@gmail.com>
2024-08-26 22:17:04 -0400

    update test

--- src/test/server_app/utils/Utils.spec.ts
+++ src/test/server_app/utils/Utils.spec.ts
@@ -1,3 +1,6 @@
+import { getRequestBody } from "../../../app/server_app/utils/Utils";
+import { IncomingMessage } from "http";
+
 const requestMock = {
     on: jest.fn(),
 };
@@ -12,7 +15,15 @@ const someObjectAsString = JSON.stringify(someObject);
 
 describe("getRequestBody test suite", () => {
     test("should return object for valid JSON", async () => {
-        requestMock.on.mockImplementation();
+        requestMock.on.mockImplementation((event, cb) => {
+            if (event == "data") {
+                cb(someObjectAsString);
+            } else {
+                cb();
+            }
+        });
+
+        const actual = await getRequestBody(requestMock as any as IncomingMessage);
     });
     test("should throw error for invalid JSON", async () => {});
     test("should throw error for unexpected error", async () => {});
