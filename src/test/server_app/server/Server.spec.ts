d84a29b93a0f7f0afe7cc8765adfe25b3cfc7ca1
hwangs12 <sungjun94@gmail.com>
2024-09-06 16:50:26 -0400

    add test

--- src/test/server_app/server/Server.spec.ts
+++ src/test/server_app/server/Server.spec.ts
@@ -1,1 +1,13 @@
-describe("Server test suite", () => {});
+import { Server } from "../../../app/server_app/server/Server";
+
+describe("Server test suite", () => {
+    let sut: Server;
+
+    beforeEach(() => {
+        sut = new Server();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+});
