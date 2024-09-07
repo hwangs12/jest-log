0ec6ce09daba87c48e970f9a50cfb1a757a0f60f
hwangs12 <sungjun94@gmail.com>
2024-09-07 16:51:47 -0400

    add test

--- src/test/server_app/server/Server.spec.ts
+++ src/test/server_app/server/Server.spec.ts
@@ -1,5 +1,11 @@
 import { Server } from "../../../app/server_app/server/Server";
 
+jest.mock("http", () => ({
+    createServer: (cb: Function) => {
+        cb();
+    },
+}));
+
 describe("Server test suite", () => {
     let sut: Server;
 
